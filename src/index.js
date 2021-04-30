7a182edc4fa769e61392a858c4eae417b516bfa4
Jefte <jefte199@gmail.com>
2021-04-30 14:57:01 -0300

    feat: create routes /start and /help

--- src/index.js
+++ src/index.js
@@ -14,3 +14,24 @@ const style_font = {
   parse_mode: 'Markdown'
 };
 
+bot.onText(/\/start/, (msg) => {
+
+  bot.sendMessage(msg.chat.id, 
+    `Olá ${msg.from.first_name}, bem vindo ao graphic covid\n/help para saber como me usar`);
+
+});
+
+bot.onText(/\/help/, (msg) => {
+
+  bot.sendMessage(msg.chat.id, 
+    `Você pode me controlar enviando estes comandos:
+    \n*FONTES*
+    \n/fonte *-* Para ver meu código e a fonte das minhas informações
+    \n*BRASIL*
+    \n/brasil *-* para informações de todo o Brasil
+    \n*MUNDO*
+    \nTenho informações somente de forma geral\n/israel\n/eua *-* Estados Unidos\n/italia\n/china
+    \nOutros paises embreve`,
+    style_font);
+
+});
